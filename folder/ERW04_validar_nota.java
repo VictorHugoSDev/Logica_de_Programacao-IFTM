a85b063c0218821f743ffaee3dc76569137f718e
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-07-19 17:52:40 -0300

    Create ERW04_validar_nota.java

---
+++ folder/ERW04_validar_nota.java
@@ -0,0 +1,10 @@
+/*
+Faça um programa em Java (utilizando while) que peça um número real(double) correspondente a uma nota, entre 0 e 10 ou entre 100 e 200, considerar os limites. 
+Mostre a mensagem, "nota invalida", caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+Entrada
+Vários números reais(double) fornecidos em sequencias, cada um representando uma possível nota.
+
+Saída
+Para cada nota imprima "nota invalida", se a nota não for válida ou imprima a própria nota, se válida, nesse caso finalize o programa.
+*/
