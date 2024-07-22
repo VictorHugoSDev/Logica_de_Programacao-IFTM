592336f59d573e068b9ca5457b91b9c7a903d160
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-07-22 14:39:22 -0300

    Create ERW05-validar_salarios.java

---
+++ folder/ERW05-validar_salarios.java
@@ -0,0 +1,12 @@
+/* 
+Faça um programa em Java(utilizando while) para ler um número indeterminado de salários de entrevistados. Para cada entrevistado será fornecido apenas o salário. O programa deve calcular a média dos salários informados. 
+Caso o salário informado tenha um valor negativo ou superior a 100000, o salário não será considerado e o programa finalizará.
+
+Entrada
+A entrada corresponde vários casos de teste. Cada caso contém um valor double correspondente ao salário.
+
+obs.: considere que teremos no mínimo um salário válido. Assim evitamos o problema da divisão por zero.
+
+Saída
+Imprima uma linha contendo a média salarial, no formato "R$ valor média salários"
+*/
